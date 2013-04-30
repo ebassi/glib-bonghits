70f28bffccdac3910e165f47ee47100257f2baf3
Emmanuele Bassi <ebassi@gnome.org>
2013-04-29 18:05:43 -0700

    docs: Add annotations for static string

--- glib-bonghits/gb-static-string.c
+++ glib-bonghits/gb-static-string.c
@@ -5,6 +5,9 @@
 
 #include <string.h>
 
+/* our storage; it's just a hash table used as a set, containing
+ * a pointer to the string gb_static_string_new() creates.
+ */
 G_LOCK_DEFINE_STATIC (interned_strings);
 static GHashTable *interned_strings = NULL;
 
@@ -18,6 +21,22 @@ release_interned_string (gpointer data)
   G_UNLOCK (interned_strings);
 }
 
+/**
+ * gb_static_string_new:
+ * @str: a string
+ *
+ * Creates a new reference counted string from @str.
+ *
+ * Reference counted strings can be used with any C string API,
+ * but they cannot be modified, or freed directly.
+ *
+ * Reference counted strings are interned: calling gb_static_string_new()
+ * with the same string will return a pointer to the same reference
+ * counted string, with its reference count increased; the string will be
+ * valid until the last reference is dropped.
+ *
+ * Return value: (transfer none): a reference counted string
+ */
 char *
 gb_static_string_new (const char *str)
 {
@@ -50,12 +69,24 @@ gb_static_string_new (const char *str)
   return res;
 }
 
+/**
+ * gb_static_string_ref:
+ * @str: a reference counted string
+ *
+ * Acquires a reference on @str.
+ */
 void
 gb_static_string_ref (char *str)
 {
   gb_ref_ptr_acquire (str);
 }
 
+/**
+ * gb_static_string_unref:
+ * @str: a reference counted string
+ *
+ * Releases a reference on @str.
+ */
 void
 gb_static_string_unref (char *str)
 {
